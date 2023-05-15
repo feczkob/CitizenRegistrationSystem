6e40576c24afa15e5c5f11d0a1b687c9f61f80b0
Botond Feczkó <feczko.botond@hallgato.ppke.hu>
2023-05-15 10:12:37 +0200

    initial commit

---
+++ src/main/kotlin/com/crs/CitizenRegistrationSystemApplication.kt
@@ -0,0 +1,11 @@
+package com.crs
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CitizenRegistrationSystemApplication
+
+fun main(args: Array<String>) {
+    runApplication<CitizenRegistrationSystemApplication>(*args)
+}
