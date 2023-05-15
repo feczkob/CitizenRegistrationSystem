fb2e4a6d31a62bf0b9b5250bca93668dda5f28df
Botond Feczkó <feczko.botond@hallgato.ppke.hu>
2023-05-15 11:22:24 +0200

    adding swagger support

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "3.0.6"
+    id("org.springframework.boot") version "2.7.1"
     id("io.spring.dependency-management") version "1.1.0"
     kotlin("jvm") version "1.7.22"
     kotlin("plugin.spring") version "1.7.22"
@@ -15,11 +15,18 @@ repositories {
     mavenCentral()
 }
 
+object Version {
+    val springdoc = "1.7.0"
+}
+
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    implementation("org.springdoc:springdoc-openapi-data-rest:${Version.springdoc}")
+    implementation("org.springdoc:springdoc-openapi-ui:${Version.springdoc}")
+    runtimeOnly("org.springdoc:springdoc-openapi-kotlin:${Version.springdoc}")
 }
 
 tasks.withType<KotlinCompile> {
--- src/main/kotlin/com/crs/CitizenRegistrationSystemApplication.kt
+++ src/main/kotlin/com/crs/CitizenRegistrationSystemApplication.kt
@@ -1,9 +1,22 @@
 package com.crs
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.info.Info
+import io.swagger.v3.oas.annotations.servers.Server
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@OpenAPIDefinition(
+    servers = [
+        Server(
+            url = "http://localhost:8080"
+        )
+    ],
+    info = Info(
+        title = "Citizen Registration System"
+    )
+)
 class CitizenRegistrationSystemApplication
 
 fun main(args: Array<String>) {
