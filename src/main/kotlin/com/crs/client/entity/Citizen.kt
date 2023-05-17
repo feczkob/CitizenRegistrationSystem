1a493904e369726e851ef46f76427453648b7474
Botond Feczkó <feczko.botond@hallgato.ppke.hu>
2023-05-17 15:49:51 +0200

    adding index init

--- src/main/kotlin/com/crs/client/entity/Citizen.kt
+++ src/main/kotlin/com/crs/client/entity/Citizen.kt
@@ -1,12 +1,12 @@
 package com.crs.client.entity
 
-import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.index.Indexed
+import org.springframework.data.mongodb.core.mapping.MongoId
 import java.time.LocalDate
 
 class Citizen(
-    @Id
-    @Indexed
+    @MongoId
+    @Indexed // * https://www.baeldung.com/spring-data-mongodb-unique
     var id: String? = null,
     var name: String = "",
     var dateOfBirth: LocalDate = LocalDate.of(1970, 1, 1),
--- src/main/resources/db/mongo-init.js
+++ src/main/resources/db/mongo-init.js
@@ -10,3 +10,10 @@ db.createUser(
         ]
     }
 );
+
+db = new Mongo().getDB("citizen");
+
+db.createCollection('citizen', { capped: false });
+
+db.citizen.createIndex({_id:1})
+db.citizen.createIndex({idNumber:1}, {unique: true})
